c0a469f993b220d8683591cbaeb608a3ccf57d8e
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-02-18 22:54:08 +0100

    [FEATURE] First tests added.

---
+++ test/collection/vector_test.go
@@ -0,0 +1,39 @@
+package collection
+
+import (
+	"testing"
+	"github.com/Rafael24595/go-collections/collection"
+)
+
+func TestSize(t *testing.T) {
+	vector := collection.VectorEmpty[int]()
+	vector.Append(0)
+
+	result := vector.Size()
+    expected := 1
+
+    if result != expected {
+        t.Errorf("Expected %d but got %d", expected, result)
+    }
+}
+
+func TestContains(t *testing.T) {
+	vector := collection.VectorEmpty[int]()
+	vector.Append(0)
+
+	resultTrue := vector.Contains(func(i int) bool {
+		return i == 0
+	})
+
+	resultFalse := vector.Contains(func(i int) bool {
+		return i == 9
+	})
+
+    if !resultTrue {
+        t.Errorf("Vector does not contains %d but it is added.", 0)
+    }
+
+	if resultFalse {
+        t.Errorf("Vector contains %d but it is not added.", 9)
+    }
+}
