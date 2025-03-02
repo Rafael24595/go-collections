cc4f6dd8d531783337cdb1df12f06b3402ae0ceb
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-03-02 23:23:45 +0100

    [FIX] Method DictionaryMap updated.
    - DictionaryMap now uses IDictionary interface.

---
+++ test/collection/dictionary_test.go
@@ -0,0 +1,32 @@
+package collection
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Rafael24595/go-collections/collection"
+)
+
+func TestDictionaryMap(t *testing.T) {
+	dict := collection.DictionaryFromMap(map[string]int{
+		"1":  1,
+		"2":  2,
+		"3":  3,
+		"4":  4,
+		"5":  5,
+		"6":  6,
+		"7":  7,
+		"8":  8,
+		"9":  9,
+		"10": 10,
+		"11": 11,
+	})
+
+	mapped := collection.DictionaryMap(dict, func(k string, v int) string {
+		return fmt.Sprintf("value-%d", v)
+	})
+
+	if value, ok := mapped.Get("1"); !ok || *value != "value-1" {
+		t.Errorf("Expected %s but got %s", "value-1", *value)
+	}
+}
